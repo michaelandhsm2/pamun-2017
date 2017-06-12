aeb26e1f704af86b4e75d65a42ddccdbfba15c67
Michael Fu <michaelandhsm2@gmail.com>
2017-06-12 08:39:54 +0800

    Chinese ver finalized

--- js/init-events.js
+++ js/init-events.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $('#bar-text').hide();
     $('ul.tabs').tabs();
+    $('.parallax').parallax();
 
    $('.ep1').matchHeight();
+
+   $('.here').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('.tap-target').tapTarget('open');
+      $('#bar-text').fadeIn();
+    }
+    else {
+        $('.tap-target').tapTarget('close');
+        $('#bar-text').fadeOut();
+    }
+   }, { offset: '80%' });
 
 
 
